82ea8cdf66bfc4bb1b10499d17c1a9579a0f1ff3
Tshepo90210 <kwenatk9@gmail.com>
2026-05-29 12:22:09 +0200

    Initialize Part 2 C# WPF Application architecture using .NET 8.0

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CyberSecurityChatBot
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
