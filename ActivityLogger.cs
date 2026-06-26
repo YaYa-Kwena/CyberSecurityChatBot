dbce09343a27afa2e07622fb2c8c805b8e42b4e1
Tshepo90210 <kwenatk9@gmail.com>
2026-06-26 17:48:41 +0200

    Feat: Add ActivityLogger service to record and limit user actions

---
+++ ActivityLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ActivityLogger
+{
+    private List<string> logs = new List<string>();
+
+    public void LogAction(string description)
+    {
+        string timestamp = DateTime.Now.ToString("HH:mm:ss");
+        logs.Add($"[{timestamp}] {description}");
+    }
+
+    public List<string> GetRecentLogs()
+    {
+        // Keeps the layout clean by grabbing up to the last 10 entries as required
+        return logs.AsEnumerable().Reverse().Take(10).Reverse().ToList();
+    }
+}
