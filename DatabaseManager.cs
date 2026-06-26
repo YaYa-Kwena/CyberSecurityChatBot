8b59fd6879a2b9e0e13df04ffd67da82ef27a0b0
Tshepo90210 <kwenatk9@gmail.com>
2026-06-26 17:34:27 +0200

    Feat: Add MySQL database infrastructure and DatabaseManager class

---
+++ DatabaseManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+public class DatabaseManager
+{
+    // Change "yourpassword" to the MySQL Root Password you created earlier
+    private string connectionString = "Server=localhost;Database=CybersecurityBotDB;Uid=root;Pwd=yourpassword;";
+
+    // Inserts a new task into the database table
+    public void AddTask(string title, string description, int? reminderDays)
+    {
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            string query = "INSERT INTO UserTasks (title, description, reminder_days) VALUES (@title, @desc, @days)";
+            using (MySqlCommand cmd = new MySqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@title", title);
+                cmd.Parameters.AddWithValue("@desc", description);
+                cmd.Parameters.AddWithValue("@days", (object)reminderDays ?? DBNull.Value);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+
+    // Fetches all uncompleted tasks from the database table
+    public DataTable GetTasks()
+    {
+        DataTable dt = new DataTable();
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            string query = "SELECT * FROM UserTasks WHERE is_completed = FALSE";
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+            {
+                adapter.Fill(dt);
+            }
+        }
+        return dt;
+    }
+}
